56eb96f2e259101fbe66708e51da176c5a82822a
Andre Jia <ajia.117@gmail.com>
2021-08-25 17:01:15 -0500

    backend mostly complete; migration scripts and database set up and set up scripts in readme

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Setup Instructions
 <code></code>
-<code></code>
-<code>docker run --rm --name pg-library -e POSTGRES_PASSWORD=docker -d -p 8008:5432 -v \<project root directory\>/postgres:/var/lib/postgresql/data postgres</code>
+
+## Server
+1. Run <code>docker pull postgres</code>.
+2. Run <code>docker run --rm --name pg-library -e POSTGRES_PASSWORD=docker -d -p 8008:5432 -v \<project root directory\>/postgres:/var/lib/postgresql/data postgres</code>.
+3. Create the database <code>library-books</code> in your postgres container. You may need to use quotation marks in generating the database.
--- server/index.js
+++ server/index.js
@@ -21,30 +21,130 @@ app.get('/api/books', (req, res) => {
       if (data.length === 0) {
         res.status(404).json({
           message: 'No books found.'
-        })
+        });
       } else {
-        res.status(200).json(data)
+        res.status(200).json(data);
       }
     })
     .catch(err => {
-      console.log(err)
-      res.status(500).send()
+      console.log(err);
+      res.status(500).send();
     })
 });
 
 //Returns a single book entry from books table, matching the bookId
 app.get('/api/books/:bookId', (req, res) => {
-
+  const parsedId = parseInt(req.params.bookId)
+  if(parsedId) {
+    knex.select('*')
+      .from('books')
+      .where('id', parsedId)
+      .then(data => {
+        if(data.length === 0) {
+          res.status(404).json({message: "No book found."});
+        } else {
+          res.status(200).json(data[0]);
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).send();
+      })
+  } else {
+    res.status(400).json({message: `Bad request: received ${req.params.bookId}`});
+  }
 });
 
-//Returns whether a particular book is already checked out
-app.get('/api/books/:bookId/checkout/:userId', (res, req) => {
+const formattedDate = (date) => {
+  return `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`;
+}
 
+//Returns whether a particular book is already checked out
+app.get('/api/books/:bookId/checkout/:userId', (req, res) => {
+  const parsedBookId = parseInt(req.params.bookId)
+  const parsedUserId = parseInt(req.params.userId)
+  if(parsedBookId && parsedUserId) {
+    knex.select('checked_out_to', 'checked_out_until')
+      .from('books')
+      .where({'id': parsedBookId})
+      .then(data => {
+        if(data.length === 0) {
+          res.status(404).json({message: "No book found."});
+        } else {
+          // build the message around which user you are
+          const checkedOutTo = data[0].checked_out_to;
+          const checkedOutUntil = new Date(data[0].checked_out_until);
+          let response = {};
+          if (!checkedOutTo) { 
+            response.message = 'This book is available for checkout.'
+            response.checkedOut = false;
+          } else if (parsedUserId === checkedOutTo) {
+            response.message = 'Book is already checked out to you.'
+            response.checkedOut = true;
+          } else {
+            response.message = `Book checked out by another user; will be available ${formattedDate(checkedOutUntil)}.`
+            response.checkedOut = true;
+          }
+          res.status(200).json(response);
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).send();
+      })
+  } else {
+    res.status(400).json({message: `Bad request: received book id: ${req.params.bookId} and user id: ${req.params.userId}`});
+  }
 });
 
 //Sets userId into checked_out_to column of a book entry from books table, matching the bookId
-app.patch('/api/books/:bookId/checkout/:userId', (res, req) => {
+app.patch('/api/books/:bookId/checkout/:userId', (req, res) => {
+  const parsedBookId = parseInt(req.params.bookId)
+  const parsedUserId = parseInt(req.params.userId)
 
+  const getDateTwoWeeksFromNow = () => {
+    let result = new Date();
+    result.setDate(result.getDate() + 14);
+    return result;
+  }
+
+  const checkoutBook = () => {
+    knex.from('books')
+      .update({
+        checked_out_to: parsedUserId, 
+        checked_out_until: getDateTwoWeeksFromNow()
+      })
+      .where({'id': parsedBookId})
+      .returning('*')
+      .then(data => {
+        res.status(200).json(data);
+      })
+      .catch(err => {
+        throw err;
+      })
+  }
+
+  if (parsedBookId && parsedUserId) {
+    //Checking whether book is available for checkout
+    knex.select('checked_out_to')
+      .from('books')
+      .where({'id': parsedBookId})
+      .then(data => {
+        if(data.length === 0) {
+          res.status(404).json({ message: "No book found." });
+        } else if(data[0].checked_out_to) {
+          res.status(403).json({message: `Book ${parsedBookId} is already checked out.`})
+        } else {
+          checkoutBook();
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).send();
+      })
+  } else {
+    res.status(400).json({message: `Bad request: received book id: ${req.params.bookId} and user id: ${req.params.userId}`});
+  }
 });
 
 //Sets checked_out_to and checked_out_until of a book entry from books table, matching the bookId
